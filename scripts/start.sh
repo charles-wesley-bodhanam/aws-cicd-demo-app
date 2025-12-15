1979feecb4f4e9cff3784b66a5106db75d3f07d3
charles-wesley-bodhanam <bcwrexams@gmail.com>
2025-12-16 01:20:06 +0530

    Update ECR repository name in start.sh script

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,16 +3,16 @@ set -e
 
 AWS_REGION=us-east-1
 ACCOUNT_ID=825765423621
-ECR_REPO=aws-cicd-demo
+ECR_REPO=aws-cicd-demo-app
 IMAGE_TAG=latest
 
 echo "Logging in to ECR..."
 aws ecr get-login-password --region $AWS_REGION | docker login \
   --username AWS \
-  --password-stdin ${825765423621}.dkr.ecr.${AWS_REGION}.amazonaws.com
+  --password-stdin ${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
 
 echo "Pulling Docker image..."
-docker pull ${825765423621}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}:${IMAGE_TAG}
+docker pull ${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}:${IMAGE_TAG}
 
 echo "Stopping old container if exists..."
 docker stop aws-cicd-app || true
@@ -22,5 +22,4 @@ echo "Starting new container..."
 docker run -d \
   --name aws-cicd-app \
   -p 80:80 \
-  ${825765423621}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}:${IMAGE_TAG}
-
+  ${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}:${IMAGE_TAG}
